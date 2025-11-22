1e74b87c04b02043665a9cd01d877de3a8161da6
Sabino Maggi <sabino.maggi@cnr.it>
2025-11-22 11:05:25 +0100

    New README and TOC

--- Formula/iconlib.rb
+++ Formula/iconlib.rb
@@ -1,8 +1,8 @@
 class Iconlib < Formula
   desc "Bash script to restore macOS application icons trapped in the squircle shape"
   homepage "https://github.com/sabinomaggi/IconLib"
-  url "https://github.com/sabinomaggi/IconLib/archive/refs/tags/v0.2.tar.gz"
-  sha256 "07545d16bee6ca9fbc82b09101bc9c9f2864489d96e99321fba4a02ca03102ea"
+  url "https://github.com/sabinomaggi/IconLib/archive/refs/tags/v0.4.tar.gz"
+  sha256 "eb78731e4476091b356a09b2a0ecd5e90a8196304c28ce059544abce3fca64bd"
   license "GPL-3.0-or-later"
 
   depends_on "fileicon"
